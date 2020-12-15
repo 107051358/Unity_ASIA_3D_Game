88032950004966d9c1cba3c7e87dced63dc45582
107051358 <72062978+107051358@users.noreply.github.com>
2020-12-15 15:59:59 +0800

    12_15有跟上老師進度

---
+++ 3DGame/Assets/Scripts/NPC.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialogue;
+    [Header("對話內容")]
+    public Text textContent;
+
+
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = true;
+            Dialogue();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = false;
+        }
+    }
+
+    private void Dialogue()
+    {
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        print("我是超難懂的迴圈:"+i)
+        //    }
+        //}
+
+        for (int i = 0; i < data.dialogueA.Length; i++)
+        {
+            print(data.dialogueA[i]);
+        }
+    }
+}
